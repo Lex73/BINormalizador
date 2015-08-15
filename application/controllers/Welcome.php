de7cd8d8db8e920a6e833e248e758435ac5b232c
Alejandro Lopez Stanley <lexls@icloud.com>
2015-08-15 20:53:39 -0300

    Preparacion para subir
    beta de prueba

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -10,68 +10,192 @@ class Welcome extends CI_Controller
 
 	public function procesar()
 	{
-			$file_i = fopen('C:\\Users\\alopez\\Documents\\'.$this->input->post('archivo'), "r");
+		if(file_exists('./assets/documents/'.$this->input->post('archivo')))
+		{
+			$file_i = fopen(base_url().'/assets/documents/'.$this->input->post('archivo'), "r");
 			$nombreCampo = array('Campo1','Campo2','Campo3','Campo4');
+			$cantidadDeCampos = 3;
 			$tipoCampo = array('Fecha','Cadena','Entero','Flotante');
-			$i=0;
-			$linea=-1;
+			$numeroLinea=-1;
 			$encabezado = true;
+			$archivo = rand();
 
 			while(!feof($file_i))
 			{
-				$linea++;
-				if ($linea > 0)
+				$linea = fgets($file_i);
+				if($linea != '')
 				{
-					$i++;
-				}
-				$campo = explode ( '|' , fgets($file_i));
-				foreach($campo as $valor)
-				{
-					//echo $encabezado;
-					if($linea > 0)
+					$numeroLinea++;
+					$campo = explode ( '|' , $linea);
+					$i=0;
+					foreach($campo as $valor)
 					{
-							echo 'tipo campo= '.$tipoCampo[$i].'</br>';
-							if($tipoCampo[$i] == 'Fecha')
-							{
-									/*$date = date_create_from_format('Y-m-d H:i:s', $valor);
-									$valor = $date->format('Y-m-d H:i:s');
-									echo $valor;*/
-							}
-							elseif ($tipoCampo[$i] == 'Cadena')
-							{
+						if($numeroLinea > 0)
+						{
+								if($tipoCampo[$i] == 'Fecha')
+								{
+									 try
+									 {
+									    $fecha = new DateTime($valor);
+											$valor = $fecha->format('Y-m-d');
+										} catch (Exception $e) {
+											try
+											{
+												$valor = str_replace('/','-',$valor);
+									    	$fecha = new DateTime($valor);
+												$valor = $fecha->format('Y-m-d');
+											}
+											catch (Exception $e)
+											{
+												$mensaje['error'] = 'Error al convertir fecha: '.$valor.' campo: '.$nombreCampo[$i].' en la linea: '.$numeroLinea;
+												echo $mensaje['error'];
+												$this->load->view('errors/errores',$mensaje);
+												unlink('./assets/documents/archivo_'.$archivo.'.txt');
+												exit();
+											}
+										}
+								}
+								elseif ($tipoCampo[$i] == 'Cadena')
+								{
+									$valor = $valor;
+								}
+								elseif ($tipoCampo[$i] == 'Entero')
+								{
+									$pos = strpos($valor, ',');
+									if($pos !== false)
+									{
+										$entero = explode ( ',' , $valor);
+										$valor = $entero[0];
+									}
+									else
+									{
+										$pos = strpos($valor, '.');
+										if($pos !== false)
+										{
+											$entero = explode ( '.' , $valor);
+											$valor = $entero[0];
+										}
+									}
 
-							}
-							elseif ($tipoCampo[$i] == 'Entero')
-							{
+									if(!is_numeric($valor))
+									{
+											$mensaje['error'] = 'Error al convertir el numero: '.$valor.' campo: '.$nombreCampo[$i].' en la linea: '.$numeroLinea;
+											echo $mensaje['error'];
+											$this->load->view('errors/errores',$mensaje);
+											unlink('./assets/documents/archivo_'.$archivo.'.txt');
+											exit();
+									}
+								}
+								elseif ($tipoCampo[$i] == 'Flotante')
+								{
+										$esNumero = true;
+									  $cambiadoACero = false;
+										$valor = str_replace(',','.',$valor);
+										$componentes = explode ( '.' , $valor);
 
-							}
-							elseif ($tipoCampo[$i] == 'Flotante')
-							{
+										if($componentes[0] == '')
+										{
+											$componentes[0] = '0';
+											$cambiadoACero = true;
+										}
 
+										try
+										{
+											$entero = (float)$componentes[0];
+											if($entero == 0)
+											{
+												if($cambiadoACero == false)
+												{
+													$esNumero = false;
+												}
+											}
+									  }
+										catch(Exception $e)
+										{
+											$mensaje['error'] = 'Error al convertir el numero: '.$valor.' campo: '.$nombreCampo[$i].' en la linea: '.$numeroLinea;
+											echo $mensaje['error'];
+											$this->load->view('errors/errores',$mensaje);
+											unlink('./assets/documents/archivo_'.$archivo.'.txt');
+											exit();
+										}
+
+										if(count($componentes) > 1)
+										{
+												try
+												{
+													if($componentes[1] != '0')
+													{
+														$decimal = (float)$componentes[1];
+														if($decimal == 0)
+														{
+																$esNumero = false;
+														}
+													}
+											  }
+												catch(Exception $e)
+												{
+													$mensaje['error'] = 'Error al convertir el numero: '.$valor.' campo: '.$nombreCampo[$i].' en la linea: '.$numeroLinea;
+													echo $mensaje['error'];
+													$this->load->view('errors/errores',$mensaje);
+													unlink('./assets/documents/archivo_'.$archivo.'.txt');
+													exit();
+												}
+									  }
+										if($esNumero == true)
+										{
+											$valor = (float)$valor;
+										}
+										else
+										{
+											$mensaje['error'] = 'Error al convertir el numero: '.$valor.' campo: '.$nombreCampo[$i].' en la linea: '.$numeroLinea;
+											echo $error;
+											$this->load->view('errors/errores',$mensaje);
+											unlink('./assets/documents/archivo_'.$archivo.'.txt');
+											exit();
+										}
+								}
+								$file_o = fopen('./assets/documents/archivo_'.$archivo.'.txt', "a");
+
+								if($i == $cantidadDeCampos)
+								{
+									fwrite($file_o,$valor);
+								}
+								else
+								{
+									fwrite($file_o,$valor.';');
+								}
+								fclose($file_o);
+								$i++;
+						}
+						else
+						{
+							$file_o = fopen('./assets/documents/archivo_'.$archivo.'.txt', "a");
+							if($i == $cantidadDeCampos)
+							{
+								fwrite($file_o,$valor);
 							}
 							else
 							{
-
+								fwrite($file_o,$valor.';');
 							}
-							echo $valor.';';
+							fclose($file_o);
 							$i++;
-							//$file_o = fopen('C:\\Users\\alopez\\Documents\\archivo_o.txt', "a");
-							//fwrite($file_o,fgets($valor).';');
-							//fclose($file_o);
-					}
-					else
-					{
-						echo $valor.';';
-						//echo 'i= '.$i.'</br>';
-						//$file_o = fopen('C:\\Users\\alopez\\Documents\\archivo_o.txt', "a");
-						//fwrite($file_o,fgets($file_i).PHP_EOL);
-						//fclose($file_o);
+						}
 					}
 				}
 			}
-			//$file_o = fopen('C:\\Users\\alopez\\Documents\\archivo_o.txt', "a");
-			//fwrite($file_o,fgets($file_i).PHP_EOL);
-			//fclose($file_o);
-			//fclose($file_i);
+			fclose($file_i);
+
+			$datos = file_get_contents('./assets/documents/archivo_'.$archivo.'.txt');
+			$nombre = 'archivo_o.txt';
+			force_download($nombre, $datos);
+		}
+		else
+		{
+			$mensaje['error'] = 'Archivo no encontrado';
+			echo $mensaje['error'];
+			$this->load->view('errors/errores',$mensaje);
+			exit();
+		}
 	}
 }
---
+++ application/views/errors/errores.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      foreach ($error as $value)
+      {
+        $mensaje = $value;
+      }
+      echo $mensaje;
+    ?>
+  </body>
+</html>
