a6e09f3db8fe9fc572d7f1c240a1230ba9789bcd
Lex73 <lexls@icloud.com>
2015-08-15 11:56:35 -0300

    Actualización del 15/8

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,77 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+class Welcome extends CI_Controller
+{
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('Pruebas/Index');
+	}
+
+	public function procesar()
+	{
+			$file_i = fopen('C:\\Users\\alopez\\Documents\\'.$this->input->post('archivo'), "r");
+			$nombreCampo = array('Campo1','Campo2','Campo3','Campo4');
+			$tipoCampo = array('Fecha','Cadena','Entero','Flotante');
+			$i=0;
+			$linea=-1;
+			$encabezado = true;
+
+			while(!feof($file_i))
+			{
+				$linea++;
+				if ($linea > 0)
+				{
+					$i++;
+				}
+				$campo = explode ( '|' , fgets($file_i));
+				foreach($campo as $valor)
+				{
+					//echo $encabezado;
+					if($linea > 0)
+					{
+							echo 'tipo campo= '.$tipoCampo[$i].'</br>';
+							if($tipoCampo[$i] == 'Fecha')
+							{
+									/*$date = date_create_from_format('Y-m-d H:i:s', $valor);
+									$valor = $date->format('Y-m-d H:i:s');
+									echo $valor;*/
+							}
+							elseif ($tipoCampo[$i] == 'Cadena')
+							{
+
+							}
+							elseif ($tipoCampo[$i] == 'Entero')
+							{
+
+							}
+							elseif ($tipoCampo[$i] == 'Flotante')
+							{
+
+							}
+							else
+							{
+
+							}
+							echo $valor.';';
+							$i++;
+							//$file_o = fopen('C:\\Users\\alopez\\Documents\\archivo_o.txt', "a");
+							//fwrite($file_o,fgets($valor).';');
+							//fclose($file_o);
+					}
+					else
+					{
+						echo $valor.';';
+						//echo 'i= '.$i.'</br>';
+						//$file_o = fopen('C:\\Users\\alopez\\Documents\\archivo_o.txt', "a");
+						//fwrite($file_o,fgets($file_i).PHP_EOL);
+						//fclose($file_o);
+					}
+				}
+			}
+			//$file_o = fopen('C:\\Users\\alopez\\Documents\\archivo_o.txt', "a");
+			//fwrite($file_o,fgets($file_i).PHP_EOL);
+			//fclose($file_o);
+			//fclose($file_i);
 	}
 }
---
+++ application/views/Pruebas/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form class="" action="<?php echo base_url(); ?>welcome/procesar" method="post">
+      <label for="">Archivo: </label>
+      <input type="text" name="archivo" value="">
+      <input type="submit" name="submit" value="Procesar">
+    </form>
+
+  </body>
+</html>
